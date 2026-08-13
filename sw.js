cbe549388cde32410baaffe633269e2e54b62329
JackDav91 <166442005+JackDav91@users.noreply.github.com>
2026-08-13 22:49:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='gengrail-log-v19.18.1-postage-accounting-clarity';
+const C='gengrail-log-v20-ui-consolidation';
 const A=['./','./index.html','./manifest.json','./gengrail-theme.css','./gengrail-ebay.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
