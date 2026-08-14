88d4bc7f55ce0f02cd59aaee0888a7d0e1fe7950
JackDav91 <166442005+JackDav91@users.noreply.github.com>
2026-08-14 15:48:50 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='gengrail-log-v20.2.0-raw-foundation';
+const C='gengrail-log-v21.2.0-raw-foundation';
 const A=['./','./index.html','./manifest.json','./gengrail-theme.css','./gengrail-ebay.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
