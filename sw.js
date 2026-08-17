0c27756860284291e0f805f30efa5e23bd04c062
JackDav91 <166442005+JackDav91@users.noreply.github.com>
2026-08-17 18:55:06 +0100

    Bump service worker cache for Grail Hub v1.10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='gengrail-log-v24.9.1-grail-identity-v18';
+const C='gengrail-log-v24.9.2-grail-identity-v110';
 const A=['./','./index.html','./manifest.json','./gengrail-theme.css','./gengrail-ebay.js','./gengrail-profit-engine.js','./profit-engine-diagnostic.js','./home-compact.css','./grail-hub.css','./grail-hub.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
